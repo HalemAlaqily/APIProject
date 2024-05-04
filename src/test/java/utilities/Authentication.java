a4299f1a2b095f8079525435b48a74c118044b08
Halema <halemaalaqiy@gmail.com>
2024-05-04 23:24:23 +0300

    update

--- src/test/java/utilities/Authentication.java
+++ src/test/java/utilities/Authentication.java
@@ -1,22 +1,37 @@
 package utilities;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
 
 import static io.restassured.RestAssured.given;
 public class Authentication {
     public static String generateToken() {
-        String strJson = """
-                {
-                    "username" : "admin",
-                    "password" : "password123"
-                }
-                """;
+        Map<String, String> bodyMap = new HashMap<>();
+        String email="";
+
+        if (email==null){
+            bodyMap.put("email", "jasonsteel@gmail.com");//If the email is null we will assign our email and password.
+            bodyMap.put("password", "Jason.123");
+        }else {
+            bodyMap.put("email", "linastraaa@gmail.com");//This email will be assigned by selenium run. If selenium does not work, this will be null.
+            bodyMap.put("password", "Linastra1420");
+        }
+
+
         Response response = given()
+                .body(bodyMap)
                 .contentType(ContentType.JSON)
-                .body(strJson)
-                .post("https://restful-booker.herokuapp.com/auth");
+                .post("https://thinking-tester-contact-list.herokuapp.com/users/login");
+//        response.prettyPrint();
 
         return response.jsonPath().getString("token");
-
     }
     }
